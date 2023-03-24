e1579dab518adb03319e3fba89bd332c8d5aecfa
Ravi <ravi.ameer@gmail.com>
2023-03-24 07:50:30 +0530

    void method example

---
+++ c6/TestVoidMethod.java
@@ -0,0 +1,29 @@
+package c6;
+import java.util.Scanner;
+public class TestVoidMethod {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter you marks. Max 100: ");
+        double score_input = input.nextDouble();
+        System.out.print("The grade is ");
+        printGrade(score_input);
+    }
+
+    public static void printGrade(double score) {
+        if (score >= 90.0) {
+            System.out.println('A');
+        }
+        else if (score >= 80.0) {
+            System.out.println('B');
+        }
+        else if (score >= 70.0) {
+            System.out.println('C');
+        }
+        else if (score >= 60.0) {
+            System.out.println('D');
+        }
+        else {
+            System.out.println("Not qualified");
+        }
+    }
+}
