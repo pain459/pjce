6b881380e1811d032d3470d095e4c72fabcb334f
Ravi <ravi.ameer@gmail.com>
2023-03-26 14:05:50 +0530

    generating random characters with methods (from other files)

---
+++ c6/TestRandomCharacter.java
@@ -0,0 +1,19 @@
+// package c6;
+
+public class TestRandomCharacter {
+    public static void main(String[] args) {
+        final int NUMBER_OF_CHARS = 175;
+        final int CHARS_PER_LINE = 25;
+
+        // print random characters between 'a' and 'z', 25 characters perf line
+        for (int i = 0; i < NUMBER_OF_CHARS; i++) {
+            char ch = RandomCharacter.getRandomLowerCaseLetter();
+            if ((i + 1) % CHARS_PER_LINE == 0) {
+                System.out.println(ch);
+            }
+            else {
+                System.out.print(ch);
+            }
+        }
+    }
+}
