fbbbebf218021c4f6d3393fae0f689e00d5e7e67
Ravi <ravi.ameer@gmail.com>
2023-03-31 08:28:52 +0530

    calculator program. End of c7.

---
+++ c7/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java calculator operand operation operand");
+            System.exit(0);
+        }
+
+        // initial result
+        int result = 0;
+        // Determine the operator
+        switch (args[1].charAt(0)) {
+            case '+' : result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+            break;
+            case '-' : result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+            break;
+            case '.' : result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+            break;
+            case '/' : result = Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
+        }
+        // Display result
+        System.out.println(args[0] + ' ' + args[1] + ' ' + args[2] + " = " + result);
+    }
+}
