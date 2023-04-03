075482be228304cdc67ec086c22e811b24279fcc
Ravi <ravi.ameer@gmail.com>
2023-04-03 23:05:12 +0530

    2.13 interest calculator

---
+++ c2_exercises/prog2_13.java
@@ -0,0 +1,23 @@
+package c2_exercises;
+
+import java.util.Scanner;
+
+public class prog2_13 {
+    public static void main(String[] args) {
+        // finance calculator
+        // yearly interest is 3.75
+        // monthly saving input in dollars.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the monthly saving amount: ");
+        double monthlySavings = input.nextDouble();
+        System.out.print("Enter number of months for calculation: ");
+        int _months = input.nextInt();
+        double annualInterest = 3.75;
+        double newBalance = 0;
+        for (int i = 1; i <= _months; i++) {
+            newBalance = (monthlySavings + newBalance) * (1 + annualInterest / 100 / 12);
+            // newBalance = (monthlySavings + newBalance) * (1 + 0.003125);
+            System.out.println("After the month " + i + ", the account value is " + newBalance);
+        }
+    }
+}
