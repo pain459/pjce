1a915e89f70b2aab371ce66942b073521985b442
Ravi <ravi.ameer@gmail.com>
2023-04-04 08:03:24 +0530

    2.18 print medians manually

---
+++ c2_exercises/prog2_18.java
@@ -0,0 +1,14 @@
+package c2_exercises;
+
+public class prog2_18 {
+    public static void main(String[] args) {
+        // calculcate mid point using given inputs.
+        System.out.println("   a        b      Middle Point");
+        System.out.println("(0, 0)   (2, 1)     (" + (2. + 0) / 2 + "," + (1. + 0) / 2 + ")");
+        System.out.println("(1, 4)   (4, 2)     (" + (4. + 1) / 2 + "," + (2. + 4) / 2 + ")");
+        System.out.println("(2, 7)   (6, 3)     (" + (6. + 2) / 2 + "," + (3. + 7) / 2 + ")");
+        System.out.println("(3, 9)   (10, 5)    (" + (10. + 3) / 2 + "," + (5. + 9) / 2 + ")");
+        System.out.println("(4, 11)  (12, 7)    (" + (12. + 4) / 2 + "," + (7. + 11) / 2 + ")");
+
+    }
+}
