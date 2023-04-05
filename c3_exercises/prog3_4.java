97774aa4b7ede39e7825fd06aac824985d19f9fb
Ravi <ravi.ameer@gmail.com>
2023-04-05 08:45:28 +0530

    3.4 random month chooser

---
+++ c3_exercises/prog3_4.java
@@ -0,0 +1,47 @@
+package c3_exercises;
+
+public class prog3_4 {
+    public static void main(String[] args) {
+        int _year = (int) (Math.random() * 10000);
+        System.out.println("Year into consideration: " + _year);
+        System.out.print("Month sorted: ");
+        switch (_year % 12) {
+            case 0:
+                System.out.println("January");
+                break;
+            case 1:
+                System.out.println("February");
+                break;
+            case 2:
+                System.out.println("March");
+                break;
+            case 3:
+                System.out.println("April");
+                break;
+            case 4:
+                System.out.println("May");
+                break;
+            case 5:
+                System.out.println("June");
+                break;
+            case 6:
+                System.out.println("July");
+                break;
+            case 7:
+                System.out.println("August");
+                break;
+            case 8:
+                System.out.println("September");
+                break;
+            case 9:
+                System.out.println("October");
+                break;
+            case 10:
+                System.out.println("November");
+                break;
+            case 11:
+                System.out.println("December");
+                break;
+        }
+    }
+}
