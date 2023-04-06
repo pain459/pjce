f21a3c174e665898a799fd2b9cf7c3154df47a58
Ravi <ravi.ameer@gmail.com>
2023-04-06 10:04:52 +0530

    3.5 adjusted logic and code cleanup

--- c3_exercises/prog3_5.java
+++ c3_exercises/prog3_5.java
@@ -8,6 +8,7 @@ public class prog3_5 {
         System.out.print("Enter the number of days elapsed since today: ");
         int daysElapsed = input.nextInt();
         int totalDays = dayToday + daysElapsed;
+        int switchDay = 0;
         if (dayToday == 0) {
             System.out.print("Today is Sunday and future day is ");
         } else if (dayToday == 1) {
@@ -24,56 +25,34 @@ public class prog3_5 {
             System.out.print("Today is Saturday and future day is ");
         } 
         if (totalDays <= 6) {
-            // System.out.print("No further calculation required.");
-            switch (totalDays) {
-                case 0:
-                    System.out.println("Sunday");
-                    break;
-                case 1:
-                    System.out.println("Monday");
-                    break;
-                case 2:
-                    System.out.println("Tuesday");
-                    break;
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                case 6:
-                    System.out.println("Saturday");
-                    break;
-            }
+            switchDay = totalDays;
         }
         else if (totalDays > 6) {
-            // System.out.print("Second loop started.");
-            switch (totalDays % 7) {
-                case 0:
-                    System.out.println("Sunday");
-                    break;
-                case 1:
-                    System.out.println("Monday");
-                    break;
-                case 2:
-                    System.out.println("Tuesday");
-                    break;
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                case 6:
-                    System.out.println("Saturday");
-                    break;
-            }
+            switchDay = totalDays % 7;
+        }
+
+        switch (switchDay) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
         }
     }
 }
