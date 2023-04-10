94e8868a318dc90da176f577037c6b82deee880a
Ravi <ravi.ameer@gmail.com>
2023-04-10 11:14:00 +0530

    contd

---
+++ c3_exercises/prog3_14.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class prog3_14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter 0 for heads and 1 for tails: ");
+        byte _choice = input.nextByte();
+        // logic starts here.
+
+    }
+}
