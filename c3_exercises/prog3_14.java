feafdb7972c39c74a059b8a17675a56d32eb2c7d
Ravi <ravi.ameer@gmail.com>
2023-04-10 13:52:50 +0530

    3.14 toss program

--- c3_exercises/prog3_14.java
+++ c3_exercises/prog3_14.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
+
 public class prog3_14 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter 0 for heads and 1 for tails: ");
         byte _choice = input.nextByte();
         // logic starts here.
+        int _toss = 0;
+        if ((int) (Math.random() * 10) >= 5) {
+            _toss = 0;
+        } else {
+            _toss = 1;
+        }
 
+        if (_toss == _choice) {
+            System.out.println("You won the toss.");
+        } else {
+            System.out.println("You lost the toss.");
+        }
     }
 }
