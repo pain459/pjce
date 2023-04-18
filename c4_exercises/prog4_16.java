ae95b2d93281e6846bec6088d8ce5d75efd1549e
Ravi <ravi.ameer@gmail.com>
2023-04-18 07:35:36 +0530

    prog4.16 random lowercase character

---
+++ c4_exercises/prog4_16.java
@@ -0,0 +1,7 @@
+public class prog4_16 {
+    public static void main(String[] args) {
+        System.out.println("Random character");
+        char _x = Character.toLowerCase((char) (int) (Math.random() * 100));
+        System.out.println(_x);
+    }
+}
