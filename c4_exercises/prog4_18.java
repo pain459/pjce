83ff955954763d872b805e445201755e066e33f5
Ravi <ravi.ameer@gmail.com>
2023-04-19 06:33:06 +0530

    prog4.18 student major and status with d and l

---
+++ c4_exercises/prog4_18.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class prog4_18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Student major and status");
+        System.out.print("Enter two characters (Combination of I,C,A and 1,2,3,4): ");
+        String _input = input.nextLine();
+        List<Character> _group = new ArrayList<Character>();
+        _group.addAll(Arrays.asList('I', 'C', 'A'));
+        List<Integer> _class = new ArrayList<Integer>();
+        _class.addAll(Arrays.asList(1, 2, 3, 4));
+        if (_group.contains(_input.charAt(0)) && _class.contains(Character.getNumericValue(_input.charAt(1)))
+                && _input.length() == 2) {
+            // System.out.println("Valid input.");
+            ;
+        } else {
+            System.out.println("Invalid input.");
+        }
+
+        Dictionary<Character, String> _classifier = new Hashtable<>(7);
+        _classifier.put('I', "Information Management");
+        _classifier.put('C', "Computer Science");
+        _classifier.put('A', "Accounting");
+        _classifier.put('1', "freshman");
+        _classifier.put('2', "sophomore");
+        _classifier.put('3', "junior");
+        _classifier.put('4', "Senior");
+
+        // parse the output
+        System.out.println(_classifier.get(_input.charAt(0)) + " " + _classifier.get(_input.charAt(1)));
+
+    };
+}
