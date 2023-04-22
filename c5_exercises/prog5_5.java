ca64fa9d68c4f27c22fd9c430bee9bd6b5a956df
Ravi <ravi.ameer@gmail.com>
2023-04-22 07:22:23 +0530

    minor change of space

--- c5_exercises/prog5_5.java
+++ c5_exercises/prog5_5.java
@@ -5,7 +5,7 @@ public class prog5_5 {
         System.out.println("Celsius  Fahrenheit  |  Fahrenheit  Celsius");
         while (chartLimit <= 10) {
             double _c = (double) chartLimit;
-            double _f =  _c * 9 / 5 + 32;
+            double _f = _c * 9 / 5 + 32;
             System.out.printf("%-7s  %10s  |  %-10s  %7s\n", _c, _f, _f, _c);
             chartLimit++;
         }
