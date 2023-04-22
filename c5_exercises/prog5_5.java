fd98f9cddbf6c82917543bbf46e7128c3e06d2c2
Ravi <ravi.ameer@gmail.com>
2023-04-22 08:28:50 +0530

    prog5.5 minor fix

--- c5_exercises/prog5_5.java
+++ c5_exercises/prog5_5.java
@@ -4,7 +4,8 @@ public class prog5_5 {
         int chartLimit = 0;
         System.out.println("Celsius  Fahrenheit  |  Fahrenheit  Celsius");
         while (chartLimit <= 10) {
-            double _c = (double) chartLimit;
+            double _temp = (double) chartLimit;
+            double _c = _temp;
             double _f = _c * 9 / 5 + 32;
             System.out.printf("%-7s  %10s  |  %-10s  %7s\n", _c, _f, _f, _c);
             chartLimit++;
