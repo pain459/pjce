5abbf08b8b10710c88e416f244cfcd9382a4d48a
Ravi <ravi.ameer@gmail.com>
2023-04-23 23:27:46 +0530

    prog5.19 in progress.

---
+++ c5_exercises/prog5_19.java
@@ -0,0 +1,7 @@
+public class prog5_19 {
+    public static void main(String[] args) {
+        System.out.println("Display numbers in a pyramid pattern");
+        int maxCube = 7;
+        // tbd. No energy remaining for the day.
+    }
+}
