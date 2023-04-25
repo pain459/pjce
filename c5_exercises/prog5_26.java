2c807e53616f7b203ee9612fcf7213bedda14a39
Ravi <ravi.ameer@gmail.com>
2023-04-25 15:30:44 +0530

    prog5.25
    looped but still not correct.

--- c5_exercises/prog5_26.java
+++ c5_exercises/prog5_26.java
@@ -1,27 +1,27 @@
-import java.math.BigInteger;
+import java.math.BigDecimal;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class prog5_26 {
     public static void main(String[] args) {
         System.out.println("Compute e");
-        BigInteger _factorial = BigInteger.ONE;
-        int _factor = 1;
-        BigInteger _result = BigInteger.ONE;
-
-        for (int i = 10000; i <= 100000; i += 10000) {
-            for (int j = i; j >= 1; j--) {
-                _factorial = _factorial.multiply(BigInteger.valueOf(j));
-                _result = _factorial.divide(BigInteger.valueOf(_factor));
+        BigDecimal e = BigDecimal.ONE;
+        BigDecimal fact = BigDecimal.ONE;
+        for (int i = 100; i <= 1000; i += 100) {
+            for (int j = 1; j <= i; j++) {
+                fact = fact.multiply(new BigDecimal(i));
+                e = e.add(BigDecimal.ONE.divide(fact, new MathContext(10000, RoundingMode.HALF_UP)));
             }
-            Float _f1 = _result.floatValue();
-            System.out.printf("%.10f\n", _f1);
-            _factorial = BigInteger.ONE;
+            System.out.println("e value for " + i + " iterations is " + e.floatValue());
+            e = BigDecimal.ONE;
         }
     }
 }
 
-// BigInteger bigInt = BigInteger.valueOf(12);
+// BigDecimal bigInt = BigDecimal.valueOf(12);
 // int regularInt = 6;
 
-// BigInteger result = bigInt.divide(BigInteger.valueOf(regularInt));
+// BigDecimal result = bigInt.divide(BigDecimal.valueOf(regularInt));
 
 // System.out.println(result); // => 2
