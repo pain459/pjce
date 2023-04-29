037aae668925a628e03465cc509870119e0a28cb
Ravi <ravi.ameer@gmail.com>
2023-04-29 09:50:06 +0530

    prog6.5
    TBD

---
+++ c6_exercises/prog6_5.java
@@ -0,0 +1,11 @@
+public class prog6_5 {
+    public static void main(String[] args) {
+        System.out.println("Sort three numbers");
+        double _x = 20.5, _y = 32.5, _z = 25.41;
+    }
+
+    public static void displaySortedNumbers(double num1, double num2, double num3) {
+        double _dummy = 0;
+        System.out.print(1);
+    }
+}
