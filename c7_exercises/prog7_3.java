a03b8a243f9517961aa984111b2671413b7e6a88
Ravi <ravi.ameer@gmail.com>
2023-05-10 16:57:03 +0530

    prog7.3
    to be fixed. check is going in recursive way for now. to fix this.

--- c7_exercises/prog7_3.java
+++ c7_exercises/prog7_3.java
@@ -1,16 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class prog7_3 {
     public static void main(String[] args) {
         System.out.println("Count occurence of numbers");
         System.out.print("Enter the integers between 1 and 50: ");
         captureInput();
-        int[] testarray = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1};
+        int[] testarray = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9 };
         repeatChecker(testarray);
     }
 
-    /**capturing user input in form of a method */
+    /** capturing user input in form of a method */
     public static void captureInput() {
         Scanner input = new Scanner(System.in);
         ArrayList<Integer> userInput = new ArrayList<Integer>();
@@ -18,13 +19,11 @@ public class prog7_3 {
             int num = input.nextInt();
             if (num == 0) {
                 break;
-            }
-            else if (num > 50) {
+            } else if (num > 50) {
                 System.out.println("Entered value above 50. Program exits now.");
                 System.exit(1);
                 // break;
-            }
-            else {
+            } else {
                 userInput.add(num);
             }
         }
@@ -36,12 +35,12 @@ public class prog7_3 {
         int[] cloneArray = new int[arrayToCheck.length];
         System.arraycopy(arrayToCheck, 0, cloneArray, 0, arrayToCheck.length);
         for (int i = 0; i < arrayToCheck.length; i++) {
-            for (int j = 0; j < cloneArray.length; j++) {
-                if (cloneArray[j] == arrayToCheck[i]) {
-                    count++;
-                }
-                System.out.println(cloneArray[j] + " occurs " + count + " times ");
+            int _temp = arrayToCheck[i];
+            for (int j = 0; j < arrayToCheck.length; j++) {
+                count += (arrayToCheck[i] == cloneArray[j] ? 1 : 0);
             }
+            System.out.println(arrayToCheck[i] + " occurs " + count + " times");
+            count = 0;
         }
     }
 }
