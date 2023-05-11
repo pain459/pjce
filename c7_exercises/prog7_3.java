dac85370968f3ce196c13ffebd9b35a75560bdcc
Ravi <ravi.ameer@gmail.com>
2023-05-11 07:24:17 +0530

    prog7.3
    repeated number counter

--- c7_exercises/prog7_3.java
+++ c7_exercises/prog7_3.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class prog7_3 {
     public static void main(String[] args) {
         System.out.println("Count occurence of numbers");
         System.out.print("Enter the integers between 1 and 50: ");
-        captureInput();
-        int[] testarray = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9 };
-        repeatChecker(testarray);
+        int[] userInput = captureInput(); // invocation for to take user input and return the array.
+        // int[] testarray = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9 };
+        repeatChecker(userInput); // feeder to check the repeated input.
     }
 
     /** capturing user input in form of a method */
-    public static void captureInput() {
+    public static int[] captureInput() {
         Scanner input = new Scanner(System.in);
         ArrayList<Integer> userInput = new ArrayList<Integer>();
         while (input.hasNextInt()) {
@@ -27,19 +26,32 @@ public class prog7_3 {
                 userInput.add(num);
             }
         }
-        System.out.println(userInput);
+        int[] returnedArray = userInput.stream().mapToInt(i -> i).toArray(); // Convert arraylist to array and return it from the method.
+        return returnedArray;
     }
 
     public static void repeatChecker(int[] arrayToCheck) {
         int count = 0;
-        int[] cloneArray = new int[arrayToCheck.length];
-        System.arraycopy(arrayToCheck, 0, cloneArray, 0, arrayToCheck.length);
+        // int[] cloneArray = new int[arrayToCheck.length];
+        // System.arraycopy(arrayToCheck, 0, cloneArray, 0, arrayToCheck.length);
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        // ArrayList<Integer> list2 = new ArrayList<Integer>();
+        int _temp1 = 0;
         for (int i = 0; i < arrayToCheck.length; i++) {
-            int _temp = arrayToCheck[i];
-            for (int j = 0; j < arrayToCheck.length; j++) {
-                count += (arrayToCheck[i] == cloneArray[j] ? 1 : 0);
+            _temp1 = arrayToCheck[i];
+            // list1.add(arrayToCheck[i]);
+            if (!list1.contains(arrayToCheck[i])) {
+                for (int j = 0; j < arrayToCheck.length; j++) {
+                    count += (_temp1 == arrayToCheck[j] ? 1 : 0);
+                }
+                if (count > 1) {
+                    System.out.println(_temp1 + " occurs " + count + " times.");
+                } else {
+                    System.out.println(_temp1 + " occurs " + count + " time.");
+                }
             }
-            System.out.println(arrayToCheck[i] + " occurs " + count + " times");
+            // _temp2 = _temp1;
+            list1.add(arrayToCheck[i]);
             count = 0;
         }
     }
