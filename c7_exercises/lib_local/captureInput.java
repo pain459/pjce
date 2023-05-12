09e3f44570512059d9a1a96e247b83e3f3dcc37d
Ravi <ravi.ameer@gmail.com>
2023-05-12 07:14:06 +0530

    prog7.5 and one more evaluator added to local_lib

--- c7_exercises/lib_local/captureInput.java
+++ c7_exercises/lib_local/captureInput.java
@@ -12,10 +12,23 @@ public class captureInput {
             int num = input.nextInt();
             if (num < 0) {
                 break;
-                // } else if (num > 50) {
-                // System.out.println("Entered value above 50. Program exits now.");
-                // System.exit(1);
-                // // break;
+            } else {
+                userInput.add(num);
+            }
+        }
+        int[] returnedArray = userInput.stream().mapToInt(i -> i).toArray(); // Convert arraylist to array and return it
+                                                                             // from the method.
+        return returnedArray;
+    }
+
+    public static int[] captureInputZeroExit() {
+        System.out.println("(Enter 0 to exit) ");
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> userInput = new ArrayList<Integer>();
+        while (input.hasNextInt()) {
+            int num = input.nextInt();
+            if (num == 0) {
+                break;
             } else {
                 userInput.add(num);
             }
---
+++ c7_exercises/lib_local/inputEvaluators.java
@@ -0,0 +1,17 @@
+package lib_local;
+
+import java.util.ArrayList;
+
+public class inputEvaluators {
+    public static int[] oddEvenCountReturn(int[] userInput) {
+        int[] results = new int[2];
+        int even = 0, odd = 0;
+        for (int i : userInput) {
+            even += (i % 2 == 0 ? 1 : 0);
+            odd += (i % 2 != 0 ? 1 : 0);
+        }
+        results[0] = even;
+        results[1] = odd;
+        return results;
+    }
+}
