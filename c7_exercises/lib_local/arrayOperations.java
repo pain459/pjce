cd018b659d772b364932208d90a901e8569443a2
Ravi <ravi.ameer@gmail.com>
2023-05-20 18:21:58 +0530

    prog7.17
    selectiion sort basic is done. rest of the program pending.

--- c7_exercises/lib_local/arrayOperations.java
+++ c7_exercises/lib_local/arrayOperations.java
@@ -1,4 +1,5 @@
 package lib_local;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,19 +18,19 @@ public class arrayOperations {
     public static int[] eliminateDuplicates(int[] userInput) {
         List<Integer> cleanedIntegers = new ArrayList<>(); // declared arraylist to add elements dynamically
         int _temp = 0;
-        for (int i = 0; i < userInput.length; i++ ){
+        for (int i = 0; i < userInput.length; i++) {
             _temp = userInput[i];
             if (!cleanedIntegers.contains(_temp)) {
                 cleanedIntegers.add(_temp); // no need to give index while adding.
             }
-        }      
+        }
         int[] returnedArray = cleanedIntegers.stream().mapToInt(i -> i).toArray();
         return returnedArray;
     }
 
-    public static boolean performLinearSearch (int[] userInput, int searchElement) {
+    public static boolean performLinearSearch(int[] userInput, int searchElement) {
         boolean found = false;
-        for (int i = 0; i < userInput.length ; i++) {
+        for (int i = 0; i < userInput.length; i++) {
             if (userInput[i] == searchElement) {
                 found = true;
             }
@@ -37,9 +38,9 @@ public class arrayOperations {
         return found;
     }
 
-    public static int performBinarySearch (int[] userInput, int searchElement) {
+    public static int performBinarySearch(int[] userInput, int searchElement) {
         int low = 0;
-        int high = userInput.length -1;
+        int high = userInput.length - 1;
         while (high >= low) {
             int mid = (low + high) / 2;
             if (searchElement < userInput[mid]) {
@@ -50,6 +51,28 @@ public class arrayOperations {
                 low = mid + 1;
             }
         }
-        return -low -1;  // high < low, key not found.
+        return -low - 1; // high < low, key not found.
+    }
+
+    public static int[] performSelectionSortInt(int[] userInput) {
+        // int[] sortedArray = new int[userInput.length];
+        // [2, 9, 5, 4, 8, 1, 6]
+        for (int i = 0; i < userInput.length - 1; i++) {
+            int currentMin = userInput[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < userInput.length; j++) {
+                if (currentMin > userInput[j]) {
+                    currentMin = userInput[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if (currentMinIndex != i) {
+                userInput[currentMinIndex] = userInput[i];
+                userInput[i] = currentMin;
+            }
+        }
+        return userInput;
     }
 }
--- c7_exercises/lib_local/captureInput.java
+++ c7_exercises/lib_local/captureInput.java
@@ -49,4 +49,28 @@ public class captureInput {
 
         return userInput;
     }
+
+    public static int[] captureArrayOfSizeInt(int size) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter " + size + " int values:");
+        int[] userInput = new int[size];
+        for (int i = 0; i < size; i++) {
+            int _input = input.nextInt();
+            userInput[i] = _input;
+        }
+
+        return userInput;
+    }
+
+    public static String[] captureStringsInArray(int size) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter " + size + " strings: ");
+        String[] userInput = new String[size];
+        for (int i = 0; i < size; i++) {
+            String _input = input.nextLine();
+            userInput[i] = _input;
+        }
+
+        return userInput;
+    }
 }
