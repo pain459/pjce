338441c65988e38315fbb4f1431127cf103ccc65
Ravi <ravi.ameer@gmail.com>
2023-05-20 10:16:36 +0530

    small adjustment for max number

--- c7_exercises/prog7_16.java
+++ c7_exercises/prog7_16.java
@@ -4,7 +4,7 @@ import lib_local.*;
 public class prog7_16 {
     public static void main(String[] args) {
         System.out.println("Execution time between linear and binary search");
-        int[] outPut = integerGenerator.generateArrayOfSizeOfNumberOfDigits(10000, 4);
+        int[] outPut = integerGenerator.generateArrayOfSizeOfNumberOfDigits(999999999, 4);
         //System.out.println(Arrays.toString(outPut)); // NOT PRINTING THIS OUTPUT AS IT WILL SPAM.
         int randomKey = integerGenerator.generateRandomIntegerOfLength(4);
         System.out.println("Generated random integer: " + randomKey);
