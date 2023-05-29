7ffef60a16e9afe00407893c68a15fa23f783088
Ravi <ravi.ameer@gmail.com>
2023-05-29 08:36:04 +0530

    TestSimpleCircle program

--- c9/TestSimpleCircle.java
+++ c9/TestSimpleCircle.java
@@ -5,41 +5,50 @@ public class TestSimpleCircle {
         // Create a circle with radius 1
         SimpleCircle circle1 = new SimpleCircle();
         System.out.println("The area of the circle of radius "
-        + circle1.radius + " is " + circle1.getArea());
+                + circle1.radius + " is " + circle1.getArea());
 
         // Create a circle with radius 25
         SimpleCircle circle2 = new SimpleCircle(25);
         System.out.println("The area of the circle of radius "
-        + circle2.radius + " is " + circle2.getArea());
+                + circle2.radius + " is " + circle2.getArea());
 
-    }    
+        // Create a circle with redius 125
+        SimpleCircle circle3 = new SimpleCircle(125);
+        System.out.println("The area of the circle of radius "
+                + circle3.radius + " is " + circle3.getArea());
+
+        // Modify circle radius
+        circle2.radius = 100;
+        System.out.println("The area of the circle of radius "
+                + circle2.radius + " is " + circle2.getArea());
+    }
 }
 
 // define a circle class with 2 constructors
 class SimpleCircle {
-    double radius;  // declaring the variable radius
+    double radius; // declaring the variable radius
 
-    /**Construct a circle with radius 1 */
+    /** Construct a circle with radius 1 */
     SimpleCircle() {
         radius = 1;
     }
 
-    /**Construct a circle with specific radius */
+    /** Construct a circle with specific radius */
     SimpleCircle(double newRadius) {
         radius = newRadius;
     }
 
-    /**Return the area of the circle */
+    /** Return the area of the circle */
     double getArea() {
         return radius * radius * Math.PI;
     }
 
-    /**Return the perimenter of the circle */
+    /** Return the perimenter of the circle */
     double getPerimeter() {
         return 2 * Math.PI * radius;
     }
 
-    /**Set a new radius for this circle */
+    /** Set a new radius for this circle */
     void setRadius(double newRadius) {
         radius = newRadius;
     }
