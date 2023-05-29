daa65bbb707b354a9ed6920131dfd6bb01abec5c
Ravi <ravi.ameer@gmail.com>
2023-05-29 22:23:05 +0530

    simplecirlce problem in single class

---
+++ c9/SimpleCircle.java
@@ -0,0 +1,30 @@
+public class SimpleCircle {
+    public static void main(String[] args) {
+        System.out.println("Demonstration of simple circle program in a single class.");
+        SimpleCircle circle1 = new SimpleCircle();
+        System.out.println("The area of the circle of radius "
+                + circle1.radius + " is " + circle1.getArea());
+    }
+
+    double radius;
+
+    SimpleCircle() {
+        radius = 1;
+    }
+
+    SimpleCircle(double newRadius) {
+        radius = newRadius;
+    }
+
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    void setRadius(double newRadius) {
+        radius = newRadius;
+    }
+}
