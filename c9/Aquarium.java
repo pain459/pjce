51af273cfd613a548ddd3ec41cbc093d331854a3
Ravi <ravi.ameer@gmail.com>
2023-06-02 06:44:24 +0530

    modifications to the aquarium program

--- c9/Aquarium.java
+++ c9/Aquarium.java
@@ -11,9 +11,33 @@ public class Aquarium {
     Aquarium() {
     }
 
-    double areaofRectangularTank(double l, double b, double h) {
+    double areaofRectangularTank() {
         double volume = (l * w * h) * 30;
         return volume; // in liters
+    }
+
+    double areaOfBowShapedTank() {
+        double square_area = l * w;
+        double eliptical_area = Math.PI * (l / 2) * (fw - w) / 2;
+        double tank_volume = ((square_area + eliptical_area) * h) * 30;
+        return tank_volume;
+    }
+
+    double areaOfCylinderShapedTank() {
+        double r = d / 2;
+        double tankVolume = Math.PI * r * r * h * 30;
+        return tankVolume;
+    }
+
+    double areaofRectangularTank(double l, double w, double h) {
+        double volume = (l * w * h) * 30;
+        return volume; // in liters
+    }
+
+    void updateDimentionsOfRectangularTank(double u_l, double u_w, double u_h) {
+        l = u_l;
+        w = u_w;
+        h = u_h;
     }
 
     double areaOfBowShapedTank(double l, double w, double fw, double h) {
@@ -23,10 +47,21 @@ public class Aquarium {
         return tank_volume;
     }
 
+    void updateDimentionsBowShapedTank(double u_l, double u_w, double u_fw, double u_h) {
+        l = u_l;
+        w = u_w;
+        fw = u_fw;
+        h = u_h;
+    }
+
     double areaOfCylinderShapedTank(double d, double h) {
         double r = d / 2;
         double tankVolume = Math.PI * r * r * h * 30;
         return tankVolume;
     }
 
+    void updateDimentionsCylinderShapedtank(double u_d, double u_h) {
+        d = u_d;
+        h = u_h;
+    }
 }
--- c9/MeasureAquariumWater.java
+++ c9/MeasureAquariumWater.java
@@ -3,11 +3,15 @@ public class MeasureAquariumWater {
         System.out.println("Aquarium water measure for differnt types.");
         System.out.println("Input units in feets.");
         Aquarium type1 = new Aquarium();
-        System.out.println("Water required to fill the rectangular aquarium with 0.5 0.5 1 dimentions is "
+        System.out.println("Water required to fill the rectangular aquarium with 0.5 0.5 1 dimensions is "
                 + type1.areaofRectangularTank(0.5, 0.5, 1) + " liters");
-        System.out.println("Water required to fill the bow tank aquarium with 2 1 1.25 2  dimentions is "
+        // updating the dimensions.
+        type1.updateDimentionsOfRectangularTank(1, 1, 2);
+        System.out.println("Water required to fill the bow tank aquarium with 2 1 1.25 2  dimensions is "
                 + type1.areaOfBowShapedTank(2, 1, 1.25, 2) + " liters");
-        System.out.println("Water required to fill the cylinder aquarium with 1 2 dimentions is "
+        System.out.println("Water required to fill the cylinder aquarium with 1 2 dimensions is "
                 + type1.areaOfCylinderShapedTank(1, 2) + " liters");
+        System.out.println("Water required to fill the rectangular aquarium with updated 1 1 2 dimensions is "
+                + type1.areaofRectangularTank() + " liters");
     }
 }
