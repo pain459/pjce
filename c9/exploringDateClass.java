61f3779ca7212800fcb91a9a3fa3c5ff0db7a17b
Ravi <ravi.ameer@gmail.com>
2023-06-04 07:44:09 +0530

    exploring date class.

---
+++ c9/exploringDateClass.java
@@ -0,0 +1,8 @@
+public class exploringDateClass {
+    public static void main(String[] args) {
+        System.out.println("Exploring date class.");
+        java.util.Date date = new java.util.Date();
+        System.out.println("Date in ms " + date.getTime());
+        System.out.println("Date in string " + date.toString());
+    }
+}
