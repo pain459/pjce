a9d221ee98e4166364e28ac37da4888cde6f7500
Ravi <ravi.ameer@gmail.com>
2023-06-07 08:23:16 +0530

    added dummy to test the effectiveness of static.

--- c9_exercises/prog9_1.java
+++ c9_exercises/prog9_1.java
@@ -13,12 +13,16 @@ public class prog9_1 {
         rectangle r2 = new rectangle(3.5, 6.8);
         System.out.println("Area of custom given rectangle: " + r2.getArea());
         System.out.println("Perimeter of custom given rectangle: " + r2.getPerimeter());
+        System.out.println("Trying to mess with non static values in rectangle class.");
+        r1.dummy = 100;
+        System.out.println("Returning dummy: " + r1.testingDummy());
     }
 }
 
 class rectangle {
     static double width = 1;
     static double height = 1;
+    double dummy = 5;
 
     // constructor
     rectangle() {
@@ -27,6 +31,7 @@ class rectangle {
     rectangle(double newWidth, double newHeight) {
         width = (newWidth > 0) ? newWidth : width;
         height = (newHeight > 0) ? newHeight : height;
+        dummy = 10;
     }
 
     double getArea(){
@@ -35,5 +40,9 @@ class rectangle {
 
     double getPerimeter() {
         return 2 * (width + height);
+    }
+
+    double testingDummy() {
+        return dummy;
     }
 }
