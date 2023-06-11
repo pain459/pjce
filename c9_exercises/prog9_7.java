e1394856833b3a4213ce3173b3dfb3e0f5efb640
Ravi <ravi.ameer@gmail.com>
2023-06-11 16:34:30 +0530

    started accounts program.

---
+++ c9_exercises/prog9_7.java
@@ -0,0 +1,11 @@
+package c9_exercises;
+
+public class prog9_7 {
+    public static void main(String[] args) {
+        System.out.println("Account class program which returns emi details.");
+    }
+}
+
+class account {
+
+}
