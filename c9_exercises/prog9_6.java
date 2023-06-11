c9f8fe55a95e033bff897a5bc85bff05ccb94353
Ravi <ravi.ameer@gmail.com>
2023-06-11 11:59:05 +0530

    stop watch program in java.

--- c9_exercises/prog9_6.java
+++ c9_exercises/prog9_6.java
@@ -2,6 +2,39 @@ package c9_exercises;
 
 public class prog9_6 {
     public static void main(String[] args) {
+
         System.out.println("Stopwatch program in JAVA");
+        stopWatch s1 = new stopWatch();
+        s1.start();
+        System.out.println("Sleeping for 5 seconds.");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // Handle if sleep is interrupted
+            e.printStackTrace();
+        }
+        System.out.println("After sleep.");
+        s1.stop();
+        System.out.println("Time taken for execution in ms: " + s1.getElapsedTime());
+
+    }
+}
+
+class stopWatch {
+    private long startTime;
+    private long endTime;
+
+    long start() {
+        startTime = System.currentTimeMillis();
+        return startTime;
+    }
+
+    long stop() {
+        endTime = System.currentTimeMillis();
+        return endTime;
+    }
+
+    long getElapsedTime() {
+        return this.endTime - this.startTime;
     }
 }
