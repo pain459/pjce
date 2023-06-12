b7d7f55d063e6b5b2fb894b2e4f43419d4d4928c
Ravi <ravi.ameer@gmail.com>
2023-06-12 21:16:09 +0530

    account class program added.

--- c9_exercises/prog9_7.java
+++ c9_exercises/prog9_7.java
@@ -3,9 +3,42 @@ package c9_exercises;
 public class prog9_7 {
     public static void main(String[] args) {
         System.out.println("Account class program which returns emi details.");
+        account a1 = new account(1, 25000);
+        System.out.println(a1.getBalanceByID(1));
+        a1.setDetails(1, 30000, 8.5);
+        System.out.println(a1.getBalanceByID(1));
+        System.out.println(a1.getAnnualInterestByID(1));
+        System.out.println(a1.getMonthlyInterestRateByID(1));
     }
 }
 
 class account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private java.util.Date dateCreated;
 
+    // no arg constructor for default account.
+    account () {
+
+    }
+    account (int id, int balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+    void setDetails (int id, double balance, double annualInterestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    double getBalanceByID (int id) {
+        return balance;
+    }
+    double getAnnualInterestByID (int id) {
+        return annualInterestRate;
+    }
+    double getMonthlyInterestRateByID (int id) {
+        return annualInterestRate / 12;
+    }
 }
