3224f4ae14979d2d639b967dfbca2ee85e419b28
Ravi <ravi.ameer@gmail.com>
2023-06-23 07:21:00 +0530

    Testing the passing methods to objects.

--- c9_revision/TestCircleWithPrivateDataFields.java
+++ c9_revision/TestCircleWithPrivateDataFields.java
@@ -12,11 +12,21 @@ public class TestCircleWithPrivateDataFields {
         System.out.println(c1.toString());
         printCircle(c1);
         printCircle(c2);
+        CircleWithPrivateDataFields c3 = new CircleWithPrivateDataFields(121);
+        printCircleDetails(c3);
+
     }
 
     public static void printCircle(CircleWithPrivateDataFields c) {
 //        c.setRadius(200);
         System.out.println(c.getNumberOfObjects());
+        System.out.println(c.getArea());
+    }
+
+    public static void printCircleDetails(CircleWithPrivateDataFields c) {
+        System.out.println("Getting details of the circle from object passed to method");
+        System.out.println(c.getNumberOfObjects());
+        System.out.println(c.getRadius());
         System.out.println(c.getArea());
     }
 }
