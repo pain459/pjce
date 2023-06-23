e4adcebb49d595cf2e944f1846f2c5b3d800316f
Ravi <ravi.ameer@gmail.com>
2023-06-23 06:44:20 +0530

    passing objects to methods.

--- c9_revision/TestCircleWithPrivateDataFields.java
+++ c9_revision/TestCircleWithPrivateDataFields.java
@@ -8,6 +8,15 @@ public class TestCircleWithPrivateDataFields {
         System.out.println(c1.getNumberOfObjects());
         CircleWithPrivateDataFields c2 = new CircleWithPrivateDataFields(10);
         System.out.println(c2.getNumberOfObjects());
+        System.out.println(c2.getArea());
         System.out.println(c1.toString());
+        printCircle(c1);
+        printCircle(c2);
+    }
+
+    public static void printCircle(CircleWithPrivateDataFields c) {
+//        c.setRadius(200);
+        System.out.println(c.getNumberOfObjects());
+        System.out.println(c.getArea());
     }
 }
