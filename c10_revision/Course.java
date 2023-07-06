a545e7dbb83f0bd6a076023004763a765623d7fc
Ravi <ravi.ameer@gmail.com>
2023-07-06 06:54:57 +0530

    adjusted the drop student class

--- c10_revision/Course.java
+++ c10_revision/Course.java
@@ -27,7 +27,28 @@ public class Course {
     }
 
     public void dropStudent(String student) {
+        String objectToRemove = student;
 
+        int indexToRemove = -1;
+        for (int i = 0; i < getNumberOfStudents(); i++) {
+            if (students[i].equals(objectToRemove)) {
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        String[] updatedNames = new String[students.length - 1];
+
+        int index = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (i != indexToRemove) {
+                updatedNames[index] = students[i];
+                index++;
+            }
+        }
+        this.students = new String[updatedNames.length];
+        System.arraycopy(updatedNames, 0, this.students, 0, updatedNames.length);
+        System.out.println("Updated students db.");
     }
 
 }
--- c10_revision/TestCourse.java
+++ c10_revision/TestCourse.java
@@ -20,6 +20,19 @@ public class TestCourse {
             System.out.print(students[i] + ", ");
         }
         System.out.println();
-        System.out.print("Number of students in course2: " + course2.getNumberOfStudents());
+        System.out.println("Number of students in course2: " + course2.getNumberOfStudents());
+
+
+
+        System.out.println("Simulation of dropping a student");
+        course1.dropStudent("Kim Smith");
+        System.out.println("Number of students in course1: " + course1.getNumberOfStudents());
+        String[] students_new = course1.getStudents();
+        for (int i = 0; i < course1.getNumberOfStudents(); i++) {
+            if (students_new[i] != null) {
+                System.out.print(students_new[i] + ", ");
+            }
+        }
+//        System.out.println(Arrays.toString(course1.getStudents()));
     }
 }
