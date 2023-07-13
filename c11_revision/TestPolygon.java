c0d6acb5c70eda8cb3e7079c11cfe95c23676188
Ravi <ravi.ameer@gmail.com>
2023-07-13 06:41:34 +0530

    Added one more calculation to polygon. 6 sides also working fine.

--- c11_revision/TestPolygon.java
+++ c11_revision/TestPolygon.java
@@ -2,7 +2,8 @@ package c11_revision;
 
 public class TestPolygon {
     public static void main(String[] args) {
-        PolygonFromSimpleGeometricObject polygon1 = new PolygonFromSimpleGeometricObject(12, 5, "Pink", true);
+//        PolygonFromSimpleGeometricObject polygon1 = new PolygonFromSimpleGeometricObject(12, 5, "Pink", true);
+        PolygonFromSimpleGeometricObject polygon1 = new PolygonFromSimpleGeometricObject(9, 6, "Pink", true);
         System.out.println(polygon1.toString());
         System.out.println("The circle inradius of given polygon is " + polygon1.getInRadius());
         System.out.println("The side length calculated is " + polygon1.getSideLength());
