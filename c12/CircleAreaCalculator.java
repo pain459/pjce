b8e208b111d2faca0261c4c9b57c079476578e3f
Ravi <ravi.ameer@gmail.com>
2023-07-25 22:59:12 +0530

    Adding few more exception rules.

--- c12/CircleAreaCalculator.java
+++ c12/CircleAreaCalculator.java
@@ -1,14 +1,17 @@
 package c12;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.UnknownFormatConversionException;
 
 public class CircleAreaCalculator {
     public static void getArea(double radius) {
         if(radius > 0) {
             double area =  Math.PI * radius * radius;
             System.out.println("Area of a circle with radius " + radius + " is " + area);
-        } else {
-                throw new InputMismatchException("Caught the junk value.");
+        } else if (radius == 0) {
+            throw new InputMismatchException("Caught the junk value.");
+        } else if (radius < 0) {
+            throw new UnsupportedOperationException("Nope! That's not a circle type data.");
         }
     }
 
@@ -20,6 +23,8 @@ public class CircleAreaCalculator {
             getArea(radius);
         } catch (InputMismatchException ex) {
             System.out.println("Exception caught.");
+        } catch (UnsupportedOperationException ex) {
+            System.out.println(ex);
         }
         System.out.println("Program completed.");
     }
