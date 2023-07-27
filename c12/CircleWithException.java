29b4b06c8ebb2ad0deb63ec29f1b11ced8a1e332
Ravi <ravi.ameer@gmail.com>
2023-07-27 07:44:44 +0530

    CircleWithException file added.

---
+++ c12/CircleWithException.java
@@ -0,0 +1,44 @@
+package c12;
+
+public class CircleWithException {
+    /** The radius of the circle */
+    private double radius;
+
+    /** The number of the objects created */
+    private static int numberOfObjects = 0;
+
+    /** Construct a circle with radius 1 */
+    public CircleWithException() {
+        this(1.0);
+    }
+
+    /** Construct a circle with specific radius */
+    public CircleWithException(double newRadius) {
+        setRadius(newRadius);
+        numberOfObjects++;
+    }
+
+    /** Return radius */
+    public double getRadius() {
+        return radius;
+    }
+
+    /** Set a new Radius */
+    public void setRadius(double newRadius) throws IllegalArgumentException {
+        if (newRadius >= 0) {
+            radius = newRadius;
+        } else {
+            throw new IllegalArgumentException("Radius cannot be negative");
+        }
+    }
+
+    /** Return numberOfObjects */
+    public static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+
+    /** Return the area of this circle */
+    public double findArea() {
+        return radius * radius * 3.14159;
+    }
+}
