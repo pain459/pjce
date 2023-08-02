f2c01e8dddcda091104403b07e0faa6c9a4a3365
pain459 <ravi.ameer@gmail.com>
2023-08-02 07:41:19 +0530

    edits to the file class.

--- c12/TestFileClass.java
+++ c12/TestFileClass.java
@@ -5,5 +5,7 @@ public class TestFileClass{
         java.io.File file = new java.io.File("image/example.txt");
         System.out.println("Absolute path is " + file.getAbsolutePath());
         System.out.println("Is file hidden? " + file.isHidden());
+        System.out.println(file.getPath());
+        System.out.println(file.length());
     }
 }
