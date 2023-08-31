3cb6261fb3e88aef10f574043c3685c27339dee7
Ravi <ravi.ameer@gmail.com>
2023-08-31 07:22:29 +0530

    writing data with auto close.

---
+++ c12/WritingDataWithAutoClose.java
@@ -0,0 +1,26 @@
+package c12;
+
+import java.io.IOException;
+
+public class WritingDataWithAutoClose {
+    public static void main(String[] args) throws IOException {
+        java.io.File file = new java.io.File("testing_file_class/scores1.txt");
+        if (file.exists()) {
+            System.out.println("File already exists.");
+            System.exit(1);
+        } else if (file.createNewFile()){
+//            file.createNewFile();
+            System.out.println("Created a new file.");
+        }
+        try (
+                // create a file
+                java.io.PrintWriter output = new java.io.PrintWriter(file);
+                ) {
+            // Write formatted output to the file.
+            output.print("RRR ");
+            output.println(99);
+            output.print("SSS ");
+            output.println(85);
+        }
+    }
+}
