9b9ef61cb23ba8c721083e365d549d4d26f4ec28
Ravi <ravi.ameer@gmail.com>
2023-08-31 06:45:58 +0530

    used different methods of the class.

--- c12/TestFileClass.java
+++ c12/TestFileClass.java
@@ -16,9 +16,16 @@ public class TestFileClass {
         } else {
             System.out.println("File2 already exists");
         }
+        System.out.println("Absolute path of file1 is " + file2.getAbsolutePath());
 
-        System.out.println("Absolute path of file1 is " + file1.getAbsolutePath());
+        // Focussing on file1 only for now.
         System.out.println("Does the file exist? " + file1.exists());
-        System.out.println("Absolute path of file2 is " + file2.getAbsolutePath());
+        System.out.println("The file has " + file1.length() + " bytes");
+        System.out.println("Can read? " + file1.canRead());
+        System.out.println("Can be written? " + file1.canWrite());
+        System.out.println("Is a directory? " + file1.isDirectory());
+        System.out.println("Is hidden? " + file1.isHidden());
+        System.out.println("Absolute path of file2 is " + file1.getAbsolutePath());
+        System.out.println("File1 last modified on " + new java.util.Date(file1.lastModified()));
     }
 }
