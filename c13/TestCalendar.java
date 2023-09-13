e7222648035e100a51d84fef91e61cfeecb07728
Ravi <ravi.ameer@gmail.com>
2023-09-13 07:20:42 +0530

    deriving the day from date.

--- c13/TestCalendar.java
+++ c13/TestCalendar.java
@@ -9,5 +9,10 @@ public class TestCalendar {
         System.out.println("Current time is " + new Date());
         System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
         System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
+
+        // Construct a calendar for specific day. We will test for August 15, 1947.
+        Calendar calendar1 = new GregorianCalendar(1947, 8, 15);
+        String[] dayNameOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println("August 15 of 1947 is a " + dayNameOfWeek[calendar1.get(Calendar.DAY_OF_WEEK) - 1]);
     }
 }
