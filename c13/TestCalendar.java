9fb6987ea2698aee752cd7224f4c4b4d5b013d1e
Ravi <ravi.ameer@gmail.com>
2023-09-13 07:11:41 +0530

    Extreme simple implementation to give date.

---
+++ c13/TestCalendar.java
@@ -0,0 +1,9 @@
+package c13;
+import java.util.*;
+
+public class TestCalendar {
+    public static void main(String[] args) {
+        // Construct a Georgian calendar for the current date and time
+        System.out.println("Current time is " + new Date());
+    }
+}
