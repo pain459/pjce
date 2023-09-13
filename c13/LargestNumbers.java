6954edaab8b0dc5a6000cbdcaecd8162150cb2fe
Ravi <ravi.ameer@gmail.com>
2023-09-13 07:02:29 +0530

    Implementation is completed.

--- c13/LargestNumbers.java
+++ c13/LargestNumbers.java
@@ -4,6 +4,13 @@ import java.math.*;
 
 public class LargestNumbers {
     public static void main(String[] args) {
+        ArrayList<Number> list = new ArrayList<>();
+        list.add(35);
+        list.add(258.65);
+        list.add(new BigInteger("556858859658485485"));
+        list.add(new BigDecimal("3.2252114521452256"));
+
+        System.out.println("The biggest number in the queue is " + getLargestNumber(list));
 
     }
 
