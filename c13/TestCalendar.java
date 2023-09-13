dad8bebb373cd21c98fcb94b3dda86526b266177
Ravi <ravi.ameer@gmail.com>
2023-09-13 07:15:42 +0530

    using calendar object to get year and month.

--- c13/TestCalendar.java
+++ c13/TestCalendar.java
@@ -1,9 +1,13 @@
 package c13;
+import java.time.Year;
 import java.util.*;
 
 public class TestCalendar {
     public static void main(String[] args) {
-        // Construct a Georgian calendar for the current date and time
+        // Construct a Gregorian calendar for the current date and time
+        Calendar calendar = new GregorianCalendar();
         System.out.println("Current time is " + new Date());
+        System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
+        System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
     }
 }
